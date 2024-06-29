c4b0190a59858ecbb860d0b0b25b5805b44d9e23
berhan <berhan5378@gmail.com>
2024-06-29 14:10:09 +0300

    Basic_Syntax

--- Basic PHP (Procedural)/Basic_Syntax.php
+++ Basic PHP (Procedural)/Basic_Syntax.php
@@ -1,4 +1,7 @@
 <?php
+
+//Basic_Syntax
+
 echo 'Hello world';   // Hello world
 print 'Hello world';  // Hello world     ' print' is return 1
 echo print 'Hello world'; // out put = 1  b/c of print is return 1 
@@ -7,4 +10,24 @@ print ('Hello world'); // Hello world
 echo 'Hello', ' ', 'world';  // Hello world    also we can use for 'print'
 echo "berhan's Invoice"; // berhan's Invoice
 echo 'berhan\'s Invoice'; // berhan's Invoice
+//variable
+$num = '12';   // correct
+$_24num='12';  // correct  it must be start from '_' or alphabet.
+
+$x=1;
+$y=$x;
+$x=3;
+echo $y; // 1
+$x=1;
+$y=&$x;
+$x=5;  
+echo $y; // 5
+
+
+$name='berhan';
+echo 'Hello $name'; // Hello $name
+echo 'Hello'.$name; // Hello berhan
+echo "Hello $name"; // Hello berhan
+echo "Hello {$name}"; // Hello berhan
 ?>
+<?= 'Hello world'  //Hello world   this equal to <?php echo 'Hello world'\?\> ?> 
