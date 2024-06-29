0d66c95e6983e68c244889c06fe73727bb2cd1db
berhan <berhan5378@gmail.com>
2024-06-29 14:07:41 +0300

    Basic_Syntax

---
+++ Basic PHP (Procedural)/Basic_Syntax.php
@@ -0,0 +1,10 @@
+<?php
+echo 'Hello world';   // Hello world
+print 'Hello world';  // Hello world     ' print' is return 1
+echo print 'Hello world'; // out put = 1  b/c of print is return 1 
+echo ('Hello world'); // Hello world
+print ('Hello world'); // Hello world
+echo 'Hello', ' ', 'world';  // Hello world    also we can use for 'print'
+echo "berhan's Invoice"; // berhan's Invoice
+echo 'berhan\'s Invoice'; // berhan's Invoice
+?>
