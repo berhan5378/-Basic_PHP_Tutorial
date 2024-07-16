2aa8eb02c9d77fdd17afb56f246b6f5b0696af0d
berhan <berhan5378@gmail.com>
2024-07-16 14:34:44 +0300

    Control Structures

---
+++ Basic_PHP_Procedural/11_Control_Structures.php
@@ -0,0 +1,12 @@
+<?php
+
+/* control structures (if / else / elseif/ else if)*/
+$score=95;
+if($score>= 90){
+  echo 'A';
+}elseif($score>=70){ // else if==elseif
+    echo 'B';
+}else{
+    echo 'c';
+}
+?>
