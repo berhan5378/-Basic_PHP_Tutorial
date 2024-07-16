d511b92c34e01b8c6dbedf1d3132125e59d03ee6
berhan <berhan5378@gmail.com>
2024-07-16 14:28:15 +0300

    7 Null data type

--- Basic_PHP_Procedural/7_Null_data_type.php
+++ Basic_PHP_Procedural/7_Null_data_type.php
@@ -6,4 +6,9 @@
 $x = null;
 echo $x;// '' 
 var_dump($x);// NULL
+var_dump(is_null($x)); // bool(true)
+var_dump($x === null); // bool(true)
+var_dump((int)$x);// int(0)
+var_dump((string)$x);// string(0)""
+var_dump((array)$x);// array(0){}
 ?>
